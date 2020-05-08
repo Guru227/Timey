87d099e97975a7423e13bb0b028ab5af6a9fe320
Gurusankar K <44615409+Guru227@users.noreply.github.com>
2020-05-08 15:38:46 +0530

    Update popup.py
    Removal of window title bar.
    Disabled alt+F4 command.

--- popup.py
+++ popup.py
@@ -1,9 +1,9 @@
 """Generate a pop-up window for Timey."""
-try:                         # Python2  
+try:                        # Python2  
     from Tkinter import *       # capital 'T'
     import Tkinter.font as font
     import thread 
-except ImportError:          # Python3
+except ImportError:         # Python3
     from tkinter import *       # lowercase 't'
     import tkinter.font as font
     import _thread as thread    # Note: backward compatibility
@@ -13,6 +13,7 @@ import time
 def alert_popup(title, message, path):
     
     root=init_window(title)
+    make_stubborn(root)
 
     #structure
     padding(root, 2, 15)
@@ -35,23 +36,32 @@ def init_window(t): #title
     box.geometry('%dx%d+%d+%d' % (w, h, x, y))
     return box
 
-def padding(box, h, f):   #height, font size
+# disable [x] and exit of tkinter window
+def disable_event():
+        pass
+    
+def make_stubborn(box):
+    box.overrideredirect(True)
+    box.protocol("WM_DELETE_WINDOW", disable_event)
+    
+def padding(box, h, f):     #height, font size
     p = Label(box, text='', height = h)
     p['font'] = font.Font(size=f)
     p.pack()
 
-def make_label(box, t, f):    #text, font size
+def make_label(box, t, f):  #text, font size
     lbl = Label(box, text=t, height = 1)
     lbl['font'] = font.Font(size=f)
     lbl.pack()
 
 #window destroy button
-def make_quit(box, txt):  #button text  
+def make_quit(box, txt):    #button text 
     b = Button(box, text=txt, command = box.destroy, width=10, state='disabled')
     b.pack()
     thread.start_new_thread(stall, (b, ))
 
-def stall(object):        #enable button after 30 sec
+#enable button after 30 sec
+def stall(object):
     print("stall running")
     time.sleep(30)
     object.config(state = 'normal')
